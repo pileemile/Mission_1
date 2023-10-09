e2cd9e4d94362dc0c6297bbb6244a5aca51a69d8
PICARD Emile <125199328+pileemile@users.noreply.github.com>
2023-10-09 18:59:52 +0200

    Add files via upload

---
+++ Mission_1/src/com/btssio/ozenne/Categorie.java
@@ -0,0 +1,9 @@
+package com.btssio.ozenne;
+
+public class Categorie {
+
+	public Categorie(int id, String nom, String code, int annee_min, int annee_max) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Mission_1/src/com/btssio/ozenne/CategorieRepository.java
@@ -0,0 +1,77 @@
+package com.btssio.ozenne;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+
+public class CategorieRepository {
+	
+	static public List<Categorie> getLesCategories(String filename) throws ParserConfigurationException, SAXException, IOException{
+	
+		List<Categorie> lesCategories = new ArrayList<Categorie>();		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document document = builder.parse(new File("C:\\categorie.xml"));
+		      
+		NodeList nodeList = document.getDocumentElement().getChildNodes();
+		for (int i = 0; i < nodeList.getLength(); i++) {
+		    Node node = nodeList.item(i);
+		    if (node.getNodeType() == Node.ELEMENT_NODE) {
+		        Element elem = (Element) node;
+		        int id = Integer.parseInt(elem.getAttribute("id"));
+		        String nom = elem.getElementsByTagName("nom").item(0).getChildNodes().item(0).getNodeValue();
+		        System.out.println(nom);
+		        String code = elem.getElementsByTagName("code").item(0).getChildNodes().item(0).getNodeValue();
+		        int annee_min = Integer.parseInt(elem.getElementsByTagName("annee_min").item(0).getChildNodes().item(0).getNodeValue());
+		        int annee_max = Integer.parseInt(elem.getElementsByTagName("annee_max").item(0).getChildNodes().item(0).getNodeValue());
+
+		         lesCategories.add(new Categorie(id,nom,code,annee_min,annee_max));
+		    }
+		}
+		       for (Categorie categ: lesCategories)
+		            System.out.println(categ.toString());
+		     
+		       
+	
+			
+		return lesCategories;
+	}
+	
+	
+	 public static void main(String[] args) {
+	
+	 }
+}
+		  
+	    
+
+	    
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
